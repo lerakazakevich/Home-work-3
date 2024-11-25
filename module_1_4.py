8d61771c0231533536562d23bc02c2232f227dae
lerakazakevich <valeriyakazakevich9@gmail.com>
2024-11-25 22:17:55 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+Count_of_homework=12
+Count_of_hours=1.5
+Course_name='Python'
+Time_for_homework=(1.5/12*60)
+print('Курс: ',Course_name,', всего задач:',Count_of_homework,', затрачено часов: ',Count_of_hours,', среднее время выполнения: ',Time_for_homework, 'минут')
